5151f48b5111223ca67c1678a5c627a667a3dacd
COBECTb <sovest@Mac-mini-Aleksandr.local>
2013-10-23 15:39:37 +0400

    Управление двумя сервами
    Добавил соединение по TCP и реализовал простой текстовый протокол
    кправления.

--- Tutorial 3/ViewController.h
+++ Tutorial 3/ViewController.h
@@ -2,7 +2,7 @@
 #import "GStreamerBackendDelegate.h"
 #import "JSAnalogueStick.h"
 
-@interface ViewController : UIViewController <GStreamerBackendDelegate,JSAnalogueStickDelegate> {
+@interface ViewController : UIViewController <GStreamerBackendDelegate,JSAnalogueStickDelegate,NSStreamDelegate> {
     IBOutlet UILabel *message_label;
     IBOutlet UIBarButtonItem *play_button;
     IBOutlet UIBarButtonItem *pause_button;
@@ -10,6 +10,10 @@
     IBOutlet UIView *video_container_view;
     IBOutlet NSLayoutConstraint *video_width_constraint;
     IBOutlet NSLayoutConstraint *video_height_constraint;
+    NSInputStream *inputStream;
+    NSOutputStream *outputStream;
+    int cam_x;
+    int cam_y;
 }
 @property (weak, nonatomic) IBOutlet JSAnalogueStick *LeftAS;
 @property (weak, nonatomic) IBOutlet JSAnalogueStick *RightAS;
--- Tutorial 3/ViewController.m
+++ Tutorial 3/ViewController.m
@@ -1,6 +1,8 @@
 #import "ViewController.h"
 #import "GStreamerBackend.h"
 #import <UIKit/UIKit.h>
+//#import <CoreServices/CFSocketStream.h>
+//#import <CoreServices/CFStream.h>
 
 @interface ViewController () {
     GStreamerBackend *gst_backend;
@@ -23,6 +25,9 @@
     play_button.enabled = FALSE;
     pause_button.enabled = FALSE;
     
+    cam_x = 25000;
+    cam_y = 25000;
+    
     /* Make these constant for now, later tutorials will change them */
     media_width = 320;
     media_height = 240;
@@ -42,6 +47,32 @@
 -(IBAction) play:(id)sender
 {
     [gst_backend play];
+    // Create socket pair:
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    CFStringRef remoteHost = CFSTR("192.168.2.1");
+    CFStreamCreatePairWithSocketToHost(NULL, remoteHost, 4444, &readStream, &writeStream);
+    inputStream = (__bridge_transfer NSInputStream *)readStream;
+    outputStream = (__bridge_transfer NSOutputStream *)writeStream;
+    [inputStream setDelegate:self];
+    
+    [outputStream setDelegate:self];
+    
+    [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+    [inputStream open];
+    
+    [outputStream open];
+    
+    NSString *response  = [NSString stringWithFormat:@"set pwm1 duty:%d period:476190\n", cam_x];
+    NSLog(response);
+	NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+	[outputStream write:[data bytes] maxLength:[data length]];
+    response  = [NSString stringWithFormat:@"set pwm0 duty:%d period:476190\n", cam_y];
+	data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+	[outputStream write:[data bytes] maxLength:[data length]];
 }
 
 /* Called when the Pause button is pressed */
@@ -93,7 +124,20 @@
 {
     if(analogueStick.number == 1)
     {
-        NSLog([NSString stringWithFormat:@"Left: %.1f , %.1f", self->_LeftAS.xValue, self->_LeftAS.yValue]);
+        NSLog([NSString stringWithFormat:@"Left: %.1f", self->_LeftAS.xValue]);
+        NSLog([NSString stringWithFormat:@"Left: %.1f", self->_LeftAS.yValue]);
+        int ncam_x=cam_x + 500*(self->_LeftAS.xValue)*-1;
+        if(ncam_x<=140000&&ncam_x>=12000) cam_x=ncam_x;
+        NSString *response  = [NSString stringWithFormat:@"set pwm1 duty:%d period:476190\n", cam_x];
+        NSLog(response);
+
+        int ncam_y=cam_y + 500*(self->_LeftAS.yValue);
+        if(ncam_y<=52000&&ncam_y>=19000) cam_y=ncam_y;
+        NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        [outputStream write:[data bytes] maxLength:[data length]];
+        response  = [NSString stringWithFormat:@"set pwm0 duty:%d period:476190\n", cam_y];
+        data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+        [outputStream write:[data bytes] maxLength:[data length]];
     }
     else
     {
